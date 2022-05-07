9f110b529fe5b5432ffafc9df011e75357b5e92b
dmbehindyou <94692377+dmbehindyou@users.noreply.github.com>
2022-05-07 12:29:23 +0300

    Диалоговый бот

--- структура архива.py
+++ структура архива.py
@@ -1,81 +1,46 @@
-import asyncio
-
 from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher import Dispatcher, FSMContext
 from aiogram.utils import executor
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from random import randrange
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
+storage = MemoryStorage()
 bot = Bot(token="5111560786:AAGQImq62wzJPza68i5jsFzDnxQLSeYMXnk")
-dp = Dispatcher(bot)
-
-btnDice = KeyboardButton("/dice")
-btnTimer = KeyboardButton("/timer")
-menuStart = ReplyKeyboardMarkup(resize_keyboard=True).add(btnDice, btnTimer)
-btnOneDie = KeyboardButton("кинуть один шестигранный кубик")
-btnTwoDie = KeyboardButton("кинуть 2 шестигранных кубика одновременно")
-btnBigDie = KeyboardButton("кинуть 20-гранный кубик")
-btnReturn = KeyboardButton("вернуться назад")
-menuDice = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True).add(btnOneDie, btnTwoDie, btnBigDie, btnReturn)
-btnHalfMinute = KeyboardButton("30 секунд")
-btnOneMinute = KeyboardButton("1 минута")
-btnFiveMinute = KeyboardButton("5 минут")
-menuTimer = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True).add(btnHalfMinute, btnOneMinute,
-                                                                       btnFiveMinute, btnReturn)
-btnClose = KeyboardButton("/close")
-menuClose = ReplyKeyboardMarkup(resize_keyboard=True).add(btnClose)
-que = {}
+dp = Dispatcher(bot, storage=storage)
 
 
-async def timer(user, time, msg):
-    await bot.send_message(user, f"Засёк {msg}", reply_markup=menuClose)
-    que[user] = time
-    for i in range(time):
-        if user not in que:
-            await bot.send_message(user, "вы успешно сбросили таймер", reply_markup=menuTimer)
-            break
-        await asyncio.sleep(1)
-    if user in que:
-        await bot.send_message(user, f"{msg} истекло", reply_markup=menuTimer)
+class Dialog(StatesGroup):
+    first_response = State()
+    second_response = State()
+
+
+@dp.message_handler(state="*", commands=["stop"])
+async def help_command(message: types.Message, state: FSMContext):
+    await bot.send_message(message.from_user.id, "Всего доброго!")
+    await state.finish()
 
 
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
-    await message.reply("старт",  reply_markup=menuStart)
+    await Dialog.first_response.set()
+    await bot.send_message(message.from_user.id, "Привет. Пройдите небольшой опрос, пожалуйста!\n"
+                           "Вы можете прервать опрос, послав команду /stop.\n"
+                           "В каком городе вы живёте?")
 
 
-@dp.message_handler(commands=["dice"])
-async def help_command(message: types.Message):
-    await message.reply("дайс", reply_markup=menuDice)
+@dp.message_handler(state=Dialog.first_response)
+async def help_command(message: types.Message, state: FSMContext):
+    if message.text == "/skip":
+        await bot.send_message(message.from_user.id, "Какая погода у вас за окном?")
+    else:
+        await bot.send_message(message.from_user.id, f"Какая погода в городе {message.text}?")
+    await Dialog.next()
 
 
-@dp.message_handler(commands=["timer"])
-async def help_command(message: types.Message):
-    await message.reply("таймер", reply_markup=menuTimer)
-
-
-@dp.message_handler(commands=["close"])
-async def help_command(message: types.Message):
-    del que[message.from_user.id]
-
-
-@dp.message_handler()
-async def main(message: types.Message):
-    msg = message.text
-    if msg == "вернуться назад":
-        await message.reply("вы вернулись назад", reply_markup=menuStart)
-    elif msg == "кинуть один шестигранный кубик":
-        await message.reply(f"{randrange(1, 6)}")
-    elif msg == "кинуть 2 шестигранных кубика одновременно":
-        await message.reply(f"{randrange(1, 6)}, {randrange(1, 6)}")
-    elif msg == "кинуть 20-гранный кубик":
-        await message.reply(f"{randrange(1, 20)}")
-    elif msg == "30 секунд":
-        await timer(message.from_user.id, 30, msg)
-    elif msg == "1 минута":
-        await timer(message.from_user.id, 60, msg)
-    elif msg == "5 минут":
-        await timer(message.from_user.id, 300, msg)
+@dp.message_handler(state=Dialog.second_response)
+async def help_command(message: types.Message, state: FSMContext):
+    await bot.send_message(message.from_user.id, f"Спасибо за участие в опросе! Всего доброго!")
+    await state.finish()
 
 
 if __name__ == '__main__':
