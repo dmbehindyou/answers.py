73652f0ded0a3fe8e5e25d607dd0ab21831241c6
dmbehindyou <94692377+dmbehindyou@users.noreply.github.com>
2022-05-07 13:26:52 +0300

    Не сделанный Бот-экскурсовод или «в греческом зале...»

--- структура архива.py
+++ структура архива.py
@@ -10,8 +10,12 @@ dp = Dispatcher(bot, storage=storage)
 
 
 class Dialog(StatesGroup):
-    first_response = State()
-    second_response = State()
+    come = State()
+    first_room = State()
+    second_room = State()
+    third_room = State()
+    fourth_room = State()
+    exit_state = State()
 
 
 @dp.message_handler(state="*", commands=["stop"])
@@ -22,24 +26,35 @@ async def help_command(message: types.Message, state: FSMContext):
 
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
-    await Dialog.first_response.set()
-    await bot.send_message(message.from_user.id, "Привет. Пройдите небольшой опрос, пожалуйста!\n"
-                           "Вы можете прервать опрос, послав команду /stop.\n"
-                           "В каком городе вы живёте?")
+    await bot.send_message(message.from_user.id, "Здравствуйте, вы можете войти в музей по команде /войти")
 
 
-@dp.message_handler(state=Dialog.first_response)
-async def help_command(message: types.Message, state: FSMContext):
-    if message.text == "/skip":
-        await bot.send_message(message.from_user.id, "Какая погода у вас за окном?")
-    else:
-        await bot.send_message(message.from_user.id, f"Какая погода в городе {message.text}?")
+@dp.message_handler(commands=["войти"])
+async def start_command(message: types.Message):
+    await Dialog.first_room.set()
+    await bot.send_message(message.from_user.id, "Добро пожаловать! Пожалуйста, сдайте верхнюю одежду в гардероб!")
+
+
+@dp.message_handler(state=Dialog.first_room)
+async def first(message: types.Message, state: FSMContext):
+    await bot.send_message(message.from_user.id, "Вы можете перейти в комнату 2 по команде /2"
+                                                 " или выйти по команде /выйти")
     await Dialog.next()
 
 
-@dp.message_handler(state=Dialog.second_response)
-async def help_command(message: types.Message, state: FSMContext):
-    await bot.send_message(message.from_user.id, f"Спасибо за участие в опросе! Всего доброго!")
+@dp.message_handler(state=Dialog.second_room)
+async def second(message: types.Message, state: FSMContext):
+    await bot.send_message(message.from_user.id, "Добро пожаловать! Пожалуйста, сдайте верхнюю одежду в гардероб!")
+    await Dialog.next()
+
+
+@dp.message_handler(state=Dialog.third_room)
+async def third(message: types.Message, state: FSMContext):
+    await Dialog.next()
+
+
+@dp.message_handler(state=Dialog.fourth_room)
+async def fourth(message: types.Message, state: FSMContext):
     await state.finish()
 
 
