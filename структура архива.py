3cf3985680d075ce724b4a1683be3cef614c2671
dmbehindyou <94692377+dmbehindyou@users.noreply.github.com>
2022-05-08 21:21:46 +0300

    Бот с картинкой

--- структура архива.py
+++ структура архива.py
@@ -1,7 +1,24 @@
 import vk_api
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import random
+from config import TOKEN, LOGIN, PASSWORD, ALBUM_ID, OWNER_ID, GROUP_ID
 
 
 def main():
+    vk_session = vk_api.VkApi(token=TOKEN)
+    longpoll = VkBotLongPoll(vk_session, GROUP_ID)
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            vk = vk_session.get_api()
+            response = vk.users.get(user_ids=event.obj.message['from_id'])
+            vk.messages.send(user_id=event.obj.message['from_id'],
+                             message=f"Привет, {response[0]['first_name']}!",
+                             attachment=f"photo{OWNER_ID}_{get_picture()}",
+                             random_id=random.randint(0, 2 ** 64),
+                             group_id=GROUP_ID)
+
+
+def get_picture():
     login, password = LOGIN, PASSWORD
     vk_session = vk_api.VkApi(login, password)
     try:
@@ -10,9 +27,11 @@ def main():
         print(error_msg)
         return
     vk = vk_session.get_api()
-    res = vk.photos.get(album_id=ALBUM_ID, owner_id=OWNER_ID, photo_sizes=1)
+    res = vk.photos.get(album_id=ALBUM_ID, owner_id=OWNER_ID)
+    ans = []
     for i in res['items']:
-        print(f"url: {i['sizes'][0]['url']}, высота: {i['sizes'][0]['height']}, ширина: {i['sizes'][0]['width']};")
+        ans.append(i['id'])
+    return ans[random.randrange(len(ans))]
 
 
 if __name__ == '__main__':
