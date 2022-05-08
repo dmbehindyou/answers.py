2b7e88092c6493fa215e645f6a7d34f46444bef0
dmbehindyou <94692377+dmbehindyou@users.noreply.github.com>
2022-05-08 16:45:08 +0300

    Бот Дата-Время

--- структура архива.py
+++ структура архива.py
@@ -1,24 +1,30 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
+import datetime
 
 
 def main():
-    vk_session = vk_api.VkApi(token=TOKEN)
-    longpoll = VkBotLongPoll(vk_session, group_id)
+    vk_session = vk_api.VkApi(token=token)
+    longpoll = VkBotLongPoll(vk_session, 180690386)
     for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
             vk = vk_session.get_api()
-            response = vk.users.get(user_ids=event.obj.message['from_id'], fields='city')
-            print(response)
-            if 'city' in response[0]:
+            flag = True
+            for i in ['время', 'число', 'дата', 'день']:
+                if i in event.obj.message['text']:
+                    resp = f"{datetime.datetime.now()} {datetime.datetime.now().strftime('%A')}"
+                    print(resp)
+                    vk.messages.send(user_id=event.obj.message['from_id'],
+                                     message=resp,
+                                     random_id=random.randint(0, 2 ** 64),
+                                     group_id=group_id)
+                    flag = False
+                    break
+            if flag:
                 vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message=f"Привет, {response[0]['first_name']}!\nКак поживает {response[0]['city']['title']}?",
-                                 random_id=random.randint(0, 2 ** 64),
-                                 group_id=group_id)
-            else:
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message=f"Привет, {response[0]['first_name']}!",
+                                 message="Напишите одно из следующих слов, чтобы "
+                                         "получить время по МСК: время, число, дата, день.",
                                  random_id=random.randint(0, 2 ** 64),
                                  group_id=group_id)
 
