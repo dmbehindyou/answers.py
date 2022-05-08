cfa52245db40f2f7fc0d9e5650b242c491f8ba95
dmbehindyou <94692377+dmbehindyou@users.noreply.github.com>
2022-05-08 21:54:52 +0300

    Бот дня недели

--- структура архива.py
+++ структура архива.py
@@ -1,7 +1,9 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
-from config import TOKEN, LOGIN, PASSWORD, ALBUM_ID, OWNER_ID, GROUP_ID
+import datetime
+from config import TOKEN, GROUP_ID
+people_stats = []
 
 
 def main():
@@ -10,28 +12,25 @@ def main():
     for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
             vk = vk_session.get_api()
-            response = vk.users.get(user_ids=event.obj.message['from_id'])
-            vk.messages.send(user_id=event.obj.message['from_id'],
-                             message=f"Привет, {response[0]['first_name']}!",
-                             attachment=f"photo{OWNER_ID}_{get_picture()}",
-                             random_id=random.randint(0, 2 ** 64),
-                             group_id=GROUP_ID)
-
-
-def get_picture():
-    login, password = LOGIN, PASSWORD
-    vk_session = vk_api.VkApi(login, password)
-    try:
-        vk_session.auth(token_only=True)
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-    vk = vk_session.get_api()
-    res = vk.photos.get(album_id=ALBUM_ID, owner_id=OWNER_ID)
-    ans = []
-    for i in res['items']:
-        ans.append(i['id'])
-    return ans[random.randrange(len(ans))]
+            if event.obj.message['from_id'] in people_stats:
+                try:
+                    a = [int(i) for i in event.obj.message['text'].split('-')]
+                    vk.messages.send(user_id=event.obj.message['from_id'],
+                                     message=f"{datetime.date(a[0], a[1], a[2]).strftime('%A')}",
+                                     random_id=random.randint(0, 2 ** 64),
+                                     group_id=GROUP_ID)
+                except:
+                    vk.messages.send(user_id=event.obj.message['from_id'],
+                                     message=f"Пожалуйста введите дату в формате YYYY-MM-DD",
+                                     random_id=random.randint(0, 2 ** 64),
+                                     group_id=GROUP_ID)
+            else:
+                people_stats.append(event.obj.message['from_id'])
+                vk.messages.send(user_id=event.obj.message['from_id'],
+                                 message="Привет,  я могу сказать в какой день недели была"
+                                         " какая-нибудь дата. Введите дату в формате YYYY-MM-DD",
+                                 random_id=random.randint(0, 2 ** 64),
+                                 group_id=GROUP_ID)
 
 
 if __name__ == '__main__':
