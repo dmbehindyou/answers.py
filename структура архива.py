2b2c0303774a522a4d07da7238df0419c8dedafb
dmbehindyou <94692377+dmbehindyou@users.noreply.github.com>
2022-05-09 14:43:27 +0300

    Таймер-бот

--- структура архива.py
+++ структура архива.py
@@ -1,22 +1,21 @@
 import discord
 from config_dc import TOKEN
-import requests
+import asyncio
 
 
 class YLBotClient(discord.Client):
-    async def on_ready(self):
-        print(f'{self.user} подключился и готов показать случайного котика (или пёсика!)')
-
-
     async def on_message(self, message):
         if message.author == self.user:
             return
-        if 'кот' in message.content.lower() or 'кошка' in message.content.lower():
-            response = requests.get("https://api.thecatapi.com/v1/images/search").json()
-            await message.channel.send(f"{response[0]['url']}")
-        elif 'пёс' in message.content.lower() or 'собач' in message.content.lower() or 'собак' in message.content.lower():
-            response = requests.get("https://dog.ceo/api/breeds/image/random").json()
-            await message.channel.send(f"{response['message']}")
+        try:
+            hours = int(message.content.split()[2])
+            minutes = int(message.content.split()[5])
+            await message.channel.send(f"The timer should start in {hours} hours and {minutes} minutes")
+            await asyncio.sleep(minutes * 60 + hours * 60 * 60)
+            await message.channel.send("Time X has come!")
+        except:
+            await message.channel.send(f"Please write your message in the format:\n"
+                                       f"set_timer in <number> hours and <number> minutes")
 
 
 intents = discord.Intents.default()
