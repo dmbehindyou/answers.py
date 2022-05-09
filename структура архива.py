c4618f80d86251de980cdf94db0ac12e16e4e45f
dmbehindyou <94692377+dmbehindyou@users.noreply.github.com>
2022-05-09 16:24:32 +0300

    (Discord) Бот-грамотей

--- структура архива.py
+++ структура архива.py
@@ -1,24 +1,72 @@
-import discord
 from config_dc import TOKEN
-import asyncio
-
-
-class YLBotClient(discord.Client):
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
-        try:
-            hours = int(message.content.split()[2])
-            minutes = int(message.content.split()[5])
-            await message.channel.send(f"The timer should start in {hours} hours and {minutes} minutes")
-            await asyncio.sleep(minutes * 60 + hours * 60 * 60)
-            await message.channel.send("Time X has come!")
-        except:
-            await message.channel.send(f"Please write your message in the format:\n"
-                                       f"set_timer in <number> hours and <number> minutes")
-
+import discord, pymorphy2
+from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.members = True
-client = YLBotClient(intents=intents)
-client.run(TOKEN)
+
+
+class RandomThings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='help_bot')
+    async def help_bot(self, ctx):
+        await ctx.send('Commands:\n'
+                       '"#!numerals" for agreement with numerals\n'
+                       '"#!alive" for define alive or not alive\n'
+                       '"#!noun" for noun case (nomn, gent, datv, accs, ablt, loct) and number state (single, plural)\n'
+                       '"#!inf" for infinitive state\n'
+                       '"#!morph" for full morphological analysis')
+
+
+    @commands.command(name='numerals')
+    async def numerals(self, ctx, first, second):
+        if first.isdigit():
+            first, second = second, first
+        morph = pymorphy2.MorphAnalyzer()
+        comment = morph.parse(first)[0]
+        await ctx.send(f"{second} {comment.make_agree_with_number(int(second)).word}")
+
+
+    @commands.command(name='alive')
+    async def alive(self, ctx, word):
+        def sprgivoy():
+            if words.tag.number == 'plur':
+                return 'Живые'
+            elif words.tag.gender == 'masc':
+                return 'Живой'
+            elif words.tag.gender == 'femn':
+                return 'Живая'
+            elif words.tag.gender == 'neut':
+                return 'Живое'
+
+        m = pymorphy2.MorphAnalyzer()
+        words = m.parse(word)[0]
+        if "NOUN" not in words.tag:
+            await ctx.send('Не существительное')
+        else:
+            if words.tag.animacy == 'anim':
+                await ctx.send(f"{word.capitalize()} {sprgivoy().lower()}")
+            else:
+                await ctx.send(f"{word.capitalize()} не {sprgivoy().lower()}")
+
+    @commands.command(name='noun')
+    async def noun(self, ctx, word, case, number):
+        m = pymorphy2.MorphAnalyzer()
+        await ctx.send(m.parse(word)[0].inflect({number[:-2], case}).word)
+
+    @commands.command(name='inf')
+    async def inf(self, ctx, word):
+        m = pymorphy2.MorphAnalyzer()
+        await ctx.send(m.parse(word)[0].normal_form)
+
+    @commands.command(name='morph')
+    async def mor(self, ctx, word):
+        m = pymorphy2.MorphAnalyzer()
+        await ctx.send(m.parse(word)[0])
+
+
+bot = commands.Bot(command_prefix='#!', intents=intents)
+bot.add_cog(RandomThings(bot))
+bot.run(TOKEN)
