b094f557499fc68321fef981585dd909e3798258
dmbehindyou <94692377+dmbehindyou@users.noreply.github.com>
2022-05-09 14:07:25 +0300

    Бот Случайный котик

--- структура архива.py
+++ структура архива.py
@@ -1,37 +1,25 @@
-import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-import random
-import datetime
-from config import TOKEN, GROUP_ID
-people_stats = []
+import discord
+from config_dc import TOKEN
+import requests
 
 
-def main():
-    vk_session = vk_api.VkApi(token=TOKEN)
-    longpoll = VkBotLongPoll(vk_session, GROUP_ID)
-    for event in longpoll.listen():
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            vk = vk_session.get_api()
-            if event.obj.message['from_id'] in people_stats:
-                try:
-                    a = [int(i) for i in event.obj.message['text'].split('-')]
-                    vk.messages.send(user_id=event.obj.message['from_id'],
-                                     message=f"{datetime.date(a[0], a[1], a[2]).strftime('%A')}",
-                                     random_id=random.randint(0, 2 ** 64),
-                                     group_id=GROUP_ID)
-                except:
-                    vk.messages.send(user_id=event.obj.message['from_id'],
-                                     message=f"Пожалуйста введите дату в формате YYYY-MM-DD",
-                                     random_id=random.randint(0, 2 ** 64),
-                                     group_id=GROUP_ID)
-            else:
-                people_stats.append(event.obj.message['from_id'])
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message="Привет,  я могу сказать в какой день недели была"
-                                         " какая-нибудь дата. Введите дату в формате YYYY-MM-DD",
-                                 random_id=random.randint(0, 2 ** 64),
-                                 group_id=GROUP_ID)
+class YLBotClient(discord.Client):
+    async def on_ready(self):
+        print(f'{self.user} подключился и готов показать случайного котика (или пёсика!)')
 
 
-if __name__ == '__main__':
-    main()
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if 'кот' in message.content.lower() or 'кошка' in message.content.lower():
+            response = requests.get("https://api.thecatapi.com/v1/images/search").json()
+            await message.channel.send(f"{response[0]['url']}")
+        elif 'пёс' in message.content.lower() or 'собач' in message.content.lower() or 'собак' in message.content.lower():
+            response = requests.get("https://dog.ceo/api/breeds/image/random").json()
+            await message.channel.send(f"{response['message']}")
+
+
+intents = discord.Intents.default()
+intents.members = True
+client = YLBotClient(intents=intents)
+client.run(TOKEN)
