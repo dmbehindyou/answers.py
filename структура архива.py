15d47981758566e5975a05d2c4f62437631900f8
dmbehindyou <94692377+dmbehindyou@users.noreply.github.com>
2022-05-10 19:04:38 +0300

    Загрузка файла

--- структура архива.py
+++ структура архива.py
@@ -1,79 +1,76 @@
-from config_dc import TOKEN, API_KEY
-import discord
-from discord.ext import commands
-from requests import request
-import emoji, random
-intents = discord.Intents.default()
-intents.members = True
-ans = set()
-emodjies = emoji.EMOJI_UNICODE_ENGLISH
-for k, v in emodjies.items():
-    ans.add(v[0])
-ans = list(ans)
-users = dict()
+from flask import Flask, url_for, request, render_template
+
+app = Flask(__name__)
 
 
-class RandomThings(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+@app.route('/load_photo', methods=['POST', 'GET'])
+def sample_file_upload():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                             <link rel="stylesheet"
+                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                             crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Отбор астронавтов</title>
+                            <style>
+                              h1, h3 {{
+                                text-align: auto;
+                              }}
+                            </style>
+                          </head>
+                          <body>
+                            <h1>Загрузка фотографии</h1>
+                            <h3>для участия в миссии</h3>
+                            <div>
+                              <form class="load_photo" method="post" enctype="multipart/form-data">
+                                 <div class="form-group">
+                                      <label for="photo">Выберите файл</label>
+                                      <img src="{url_for('static', filename='img/image.jpg')}" alt="Хе-хе, а картинки-то нет.">
+                                      <input type="file" class="form-control-file" id="photo" name="file">
+                                 </div>
+                                 <button type="submit" class="btn btn-primary">Отправить</button>
+                              </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        f = request.files['file'].read()
+        fd = open("static/img/image.jpg", "wb")
+        fd.write(f)
+        fd.close()
+        return f'''<!doctype html>
+                                <html lang="en">
+                                  <head>
+                                    <meta charset="utf-8">
+                                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                     <link rel="stylesheet"
+                                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                                     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                                     crossorigin="anonymous">
+                                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                                    <title>Отбор астронавтов</title>
+                                  </head>
+                                  <body>
+                                    <h1>Загрузка фотографии</h1>
+                                    <h3>для участия в миссии</h3>
+                                    <div>
+                                      <form class="load_photo" method="post" enctype="multipart/form-data">
+                                         <div class="form-group">
+                                              <label for="photo">Выберите файл</label>
+                                              <img src="{url_for('static', filename='img/image.jpg')}" alt="Хе-хе, а картинки-то нет.">
+                                              <input type="file" class="form-control-file" id="photo" name="file">
+                                         </div>
+                                         <button type="submit" class="btn btn-primary">Отправить</button>
+                                      </form>
+                                    </div>
+                                  </body>
+                                </html>'''
 
-    @commands.command(name='start')
-    async def start(self, ctx):
-        random.shuffle(ans)
-        smiles_in_game = ans[:random.randrange(50, 150)]
-        users[str(ctx.author)] = [smiles_in_game, 0, 0]
-        await ctx.send(f'The game started')
 
-    @commands.command(name='stop')
-    async def forecast(self, ctx):
-        del users[str(ctx.author)]
-        await ctx.send("You can play again")
-
-    async def on_message(self, message):
-        if message.author == self.bot.user:
-            return
-        try:
-            y = users[str(message.author)][1]
-            b = users[str(message.author)][2]
-            if len(users[str(message.author)][0]) <= 1:
-                await message.dm_channel.send(f"Emoticons are over\n"
-                                              f"Score: You {y} Bot {b}\n"
-                                              f"{'You' if y > b else 'Bot'} win!")
-                del users[str(message.author)]
-            else:
-                number = int(message.content)
-                if number >= len(users[str(message.author)][0]):
-                    your_emoji = users[str(message.author)][0][len(users[str(message.author)][0]) % number]
-                    bot_emoji = random.randrange(len(users[str(message.author)][0]))
-                    if your_emoji > users[str(message.author)][0][bot_emoji]:
-                        users[str(message.author)][1] += 1
-                    else:
-                        users[str(message.author)][2] += 1
-                    y = users[str(message.author)][1]
-                    b = users[str(message.author)][2]
-                    await message.dm_channel.send(f"Your emoji {your_emoji}\n"
-                                                  f"Bot emoji {users[str(message.author)][0][bot_emoji]}\n"
-                                                  f"Score: You {y} Bot {b}\n")
-                    del users[str(message.author)][0][len(users[str(message.author)][0]) % number]
-                    del users[str(message.author)][0][bot_emoji - 1]
-                else:
-                    your_emoji = users[str(message.author)][0][number]
-                    bot_emoji = random.randrange(len(users[str(message.author)][0]))
-                    if your_emoji > users[str(message.author)][0][bot_emoji]:
-                        users[str(message.author)][1] += 1
-                    else:
-                        users[str(message.author)][2] += 1
-                    y = users[str(message.author)][1]
-                    b = users[str(message.author)][2]
-                    await message.dm_channel.send(f"Your emoji {your_emoji}\n"
-                                                  f"Bot emoji {users[str(message.author)][0][bot_emoji]}\n"
-                                                  f"Score: You {y} Bot {b}\n")
-                    del users[str(message.author)][0][number]
-                    del users[str(message.author)][0][bot_emoji - 1]
-        except:
-            await message.dm_channel.send("Please, send correct number")
-
-
-bot = commands.Bot(command_prefix='#!', intents=intents)
-bot.add_cog(RandomThings(bot))
-bot.run(TOKEN)
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
