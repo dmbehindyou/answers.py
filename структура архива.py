70fc0673e0a2a356240b9e4bbcf403769f6ea4aa
dmbehindyou <94692377+dmbehindyou@users.noreply.github.com>
2022-05-10 13:57:51 +0300

    (Discord) Бот-метеоролог

--- структура архива.py
+++ структура архива.py
@@ -1,11 +1,11 @@
-from config_dc import TOKEN
+from config_dc import TOKEN, API_KEY
 import discord
 from discord.ext import commands
-import requests
-
+from requests import request
+from pprint import pprint
 intents = discord.Intents.default()
 intents.members = True
-peoples = dict()
+places = dict()
 
 
 class RandomThings(commands.Cog):
@@ -15,36 +15,71 @@ class RandomThings(commands.Cog):
     @commands.command(name='help_bot')
     async def help_bot(self, ctx):
         await ctx.send('Commands:\n'
-                       '"!!set_lang" for change languages, default: en-ru\n'
-                       '"!!text" for give translated text')
+                       '"#!place" for change weather forecast location\n'
+                       '"#!current" for give a now weather forecast\n'
+                       '"#!forecast <day>" to get weather forecast for several days')
 
-    @commands.command(name='set_lang')
-    async def set_lang(self, ctx, languages):
-        peoples[str(ctx.author)] = "|".join(languages.split('-'))
-        await ctx.send(f'Type "!!text" and text for translate')
+    @commands.command(name='place')
+    async def set_place(self, ctx, place):
+        places[str(ctx.author)] = place
+        await ctx.send(f'Place changed to {place}')
 
-    @commands.command(name='text')
-    async def mor(self, ctx, *words):
-        if str(ctx.author) not in peoples:
-            url = "https://translated-mymemory---translation-memory.p.rapidapi.com/api/get"
-            querystring = {"langpair": "en|ru", "q": " ".join(words), "mt": "1", "onlyprivate": "0", "de": "a@b.c"}
-            headers = {
-                "X-RapidAPI-Host": "translated-mymemory---translation-memory.p.rapidapi.com",
-                "X-RapidAPI-Key": "a4a800dbfcmsh5109ca6d95d73e7p1cef61jsnbeee0db76130"
-            }
-            response = requests.request("GET", url, headers=headers, params=querystring).json()
-            await ctx.send(response["responseData"]['translatedText'])
+    @commands.command(name='current')
+    async def current(self, ctx):
+        if str(ctx.author) not in places:
+            await ctx.send("Please, select the location of the weather forecast")
         else:
-            url = "https://translated-mymemory---translation-memory.p.rapidapi.com/api/get"
-            querystring = {"langpair": peoples[str(ctx.author)], "q": " ".join(words), "mt": "1", "onlyprivate": "0", "de": "a@b.c"}
-            headers = {
-                "X-RapidAPI-Host": "translated-mymemory---translation-memory.p.rapidapi.com",
-                "X-RapidAPI-Key": "a4a800dbfcmsh5109ca6d95d73e7p1cef61jsnbeee0db76130"
-            }
-            response = requests.request("GET", url, headers=headers, params=querystring).json()
-            await ctx.send(response["responseData"]['translatedText'])
+            response = request(
+                method='GET',
+                url='http://api.weatherstack.com/current',
+                params={
+                    'access_key': API_KEY,
+                    'query': places[str(ctx.author)]
+                }
+            )
+            if response.status_code == 200:
+                response = response.json()
+                d, t = response["location"]["localtime"].split()
+                await ctx.send(f'Current weather in {response["location"]["name"]} today {d} at time {t}:\n'
+                               f'Temperature: {response["current"]["temperature"]},\n'
+                               f'Pressure: {response["current"]["pressure"]},\n'
+                               f'Humidity: {response["current"]["humidity"]},\n'
+                               f'{response["current"]["weather_descriptions"][0]},\n'
+                               f'Wind {response["current"]["wind_dir"]}, {response["current"]["wind_speed"]} m/s.')
+            else:
+                await ctx.send("Please select the existing location")
+
+    @commands.command(name='forecast')
+    async def forecast(self, ctx, days):
+        if str(ctx.author) not in places:
+            await ctx.send("Please, select the location of the weather forecast")
+        else:
+            response = request(
+                method='GET',
+                url='http://api.weatherstack.com/current',
+                params={
+                    'access_key': API_KEY,
+                    'query': places[str(ctx.author)],
+                    'forecast_days': int(days),
+                    'hourly': 1
+                }
+            )
+            if response.status_code == 200:
+                response = response.json()
+                ans = ''
+                pprint(response)
+                for i in response["forecast"]:
+                    ans += f'Weather forecast in {response["location"]["name"]} for {response["forecast"][i]["date"]}:\n' + \
+                           f'Temperature: {response["forecast"][i]["avgtemp"]},\n' + \
+                           f'Pressure: {response["forecast"][i]["hourly"][0]["pressure"]},\n' + \
+                           f'Humidity: {response["forecast"][i]["hourly"][0]["humidity"]},\n' + \
+                           f'{response["forecast"][i]["hourly"][0]["weather_descriptions"][0]},\n' + \
+                           f'Wind {response["forecast"][i]["hourly"][0]["wind_dir"]}, {response["forecast"][i]["hourly"][0]["wind_speed"]} m/s.\n\n'
+                await ctx.send(ans)
+            else:
+                await ctx.send("Please select the existing location")
 
 
-bot = commands.Bot(command_prefix='!!', intents=intents)
+bot = commands.Bot(command_prefix='#!', intents=intents)
 bot.add_cog(RandomThings(bot))
 bot.run(TOKEN)
